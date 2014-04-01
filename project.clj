b61b17503db94c47f3f6e2fc25c0b9f014b3e32a
Andrew Nguyen <andrew-git@na-consulting.net>
2014-04-01 10:23:58 -0700

    Added basic unzipping function

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject lein-unpack-resources "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Fetches a maven-based jar and unpacks it and adds it as a resource"
+  :url "https://github.com/andrew-nguyen/lein-unpack-resources"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/java.classpath "0.2.2"]
+                 [net.lingala.zip4j/zip4j "1.3.2"]]
   :eval-in-leiningen true)
--- src/leiningen/unpack_resources.clj
+++ src/leiningen/unpack_resources.clj
@@ -1,6 +1,41 @@
-(ns leiningen.unpack-resources)
+(ns leiningen.unpack-resources
+  (:require [clojure.java.classpath :as cp]
+            [clojure.pprint :as pprint]
+            [leiningen.deps :refer [deps]]
+            [leiningen.jar :as lein.jar])
+  (:import [net.lingala.zip4j.core ZipFile]
+           [net.lingala.zip4j.exception ZipException]))
+
+(defn jar-name
+  [group artifact version ext]
+  (if (and group artifact)
+    (str group "-" artifact "-" version "." ext)
+    (str group "-" version "." ext)
+    ))
+
+(defn get-jar-files  [project]
+    (filter (memfn isFile) (deps project)))
+
+(defn unzip-file
+  [zip dest]
+  (try
+    (-> (ZipFile. zip)
+        (.extractAll dest))
+    (catch ZipException e
+      (.printStackTrace e)
+      e)))
 
 (defn unpack-resources
-  "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  (let [[group-artifact version] (get-in project [:unpack-resources :resource])
+        extract-path (get-in project [:unpack-resources :extract-path])
+        [group artifact] (clojure.string/split (name group-artifact) #"/")
+        classpath-jars (get-jar-files project)
+        jar-name (jar-name group artifact version "jar")
+        jar-file (first (filter #(.endsWith (.getName %) jar-name) classpath-jars))
+        jar-file-path (.getAbsolutePath jar-file)
+        ]
+    (println "Extracting:  " jar-file-path)
+    (println "Destination: " extract-path)
+    (unzip-file jar-file-path extract-path) 
+    ))
